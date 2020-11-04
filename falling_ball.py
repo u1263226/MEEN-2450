0993a0484d9df6a002bce33fe90179da445f7fc1
u1263226 <70610472+u1263226@users.noreply.github.com>
2020-11-04 00:14:29 -0700

    Update falling_ball.py

--- falling_ball.py
+++ falling_ball.py
@@ -8,26 +8,30 @@ ME EN 2450: Lab 7: falling_ball
 import matplotlib.pyplot as plt
 
 ball_type = ["steel","wood","pingpong"]
-m = [0.225,0.021,0.002]
-r = [0.038,0.0367,0.0374]
-params1 = [9.81,1.2,m[0],r[0],0.4]
+m = [0.225,0.021,0.002] #varying mass kg
+d = [0.038,0.0367,0.0374] #varying diameter m
+r = 0.5*d # varying radius m
+params1 = [9.81,1.2,m[0],r[0],0.4] #constants gravity, air density, mass, radius, drag coefficient
 params2 = [9.81,1.2,m[1],r[1],0.4]
 params3 = [9.81,1.2,m[2],r[2],0.4]
 tspan = []
-for i in range(1000000):
-    tspan.append(i*0.001)
+for i in range(1000000): # time range
+    tspan.append(i*0.001) # time step
 y0 = 0
 
+#define function handles for each ball
 from ball_motion import ball_motion
 odefun1 = lambda t, y: ball_motion(t,y,params1)
 odefun2 = lambda t, y: ball_motion(t,y,params2)
 odefun3 = lambda t, y: ball_motion(t,y,params3)
 
+#calculate the various velocities and corresponding times
 from eullers import eulers
 [t1,y1] = eulers(odefun1, tspan, y0)
 [t2,y2] = eulers(odefun2, tspan, y0)
 [t3,y3] = eulers(odefun3, tspan, y0)
 
+#plot velocity as a function of time and the terminal velocity for each ball
 plt.plot(t1,y1,label = "Steel")
 plt.plot(t2,y2,label = "Wood")
 plt.plot(t3,y3, label = "Ping-pong")
